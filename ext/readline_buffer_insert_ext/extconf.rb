10355ff529ef9c232eefc9903f2e65c07c3da532
Lin Jen-Shin <godfat@godfat.org>
2011-08-18 21:09:28 +0800

    update messages

--- ext/readline_buffer_insert_ext/extconf.rb
+++ ext/readline_buffer_insert_ext/extconf.rb
@@ -6,5 +6,5 @@ have_library('readline')
 if have_header('readline/readline.h')
   create_makefile 'readline_buffer_insert_ext'
 else
-  abort '--without-readline'
+  abort 'Readline not found.'
 end
