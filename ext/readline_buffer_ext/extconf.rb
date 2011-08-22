b7e5751cb8591dd5a107ab1b75677e67ec220773
Lin Jen-Shin <godfat@godfat.org>
2011-08-22 22:35:02 +0800

    try if this would fix #1 ? this is so weird. see:
    conftest.c:5:1: error: too few arguments to function 'rl_delete_text'
    
    http://www.ruby-forum.com/topic/1354865
    https://github.com/tenderlove/nokogiri/blob/master/ext/nokogiri/extconf.rb

--- ext/readline_buffer_ext/extconf.rb
+++ ext/readline_buffer_ext/extconf.rb
@@ -4,8 +4,11 @@ dir_config('readline')
 have_library('readline')
 
 if have_header('readline/readline.h')
-  if have_func('rl_delete_text', 'readline/readline.h')
+  if have_func('rl_delete_text', 'readline/readline.h') ||
+     have_library('readline', 'rl_delete_text', 'readline/readline.h')
+
     create_makefile 'readline_buffer_ext'
+
   else
     abort 'Only Readline (not EditLine) is supported'
   end
